5a35dcacc954efda2d5351079d846c36839e7ea6
Na-aim Fredericks <fredericksnaaim13@gmail.com>
2021-11-26 15:31:38 +0200

    update files

---
+++ index.js
@@ -0,0 +1,23 @@
+// functions
+let sum = "";
+
+function addtosum(operator) {
+  sum = sum + operator;
+  console.log(sum);
+  document.getElementById("output").innerText = sum;
+}
+
+function evaluateSum() {
+  sum = eval(sum);
+  document.getElementById("output").innerText = sum;
+}
+
+function clearSum() {
+  sum = "";
+  document.getElementById("output").innerText = sum;
+}
+
+function deleteChar() {
+  sum = sum.slice(0, -1);
+  document.getElementById("output").innerText = sum;
+}
